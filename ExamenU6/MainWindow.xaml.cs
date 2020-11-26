38b2b9dc71e44234bd435374320f6f50f20ff134
cristian <Cristian_caht@hotmail.com>
2020-11-25 19:24:29 -0600

    prueba

--- ExamenU6/MainWindow.xaml.cs
+++ ExamenU6/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace ExamenU6
         {
             InitializeComponent();
             List<Book> Books = new List<Book>();
-            Books.Add(new Book() { Title = "Harry Potter", Author = "JK Rowling", Editorial = "Salamandra", Taken = true, OwnedBy = "Jugo" });
+            Books.Add(new Book() { Title = "Razer y la patatus del poder", Author = "JK Rowling", Editorial = "Salamandra", Taken = true, OwnedBy = "Jugo" });
             Books.Add(new Book() { Title = "El juego de ender", Author = "JK Rowling", Editorial = "Salamandra", Taken = false, OwnedBy = "" });
             Books.Add(new Book() { Title = "Harry Potter", Author = "JK Rowling", Editorial = "Salamandra", Taken = true, OwnedBy = "Jugo" });
             ListBoxBooks.ItemsSource = Books; 
